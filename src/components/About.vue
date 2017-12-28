1806e366837b96501c9fb41d9706b1e5f69701b4
Dmitriy Klyuzov <dmitriy.klyuzov@gmail.com>
2017-12-28 00:51:47 -0500

    added 'About' component

---
+++ src/components/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about container">
+    <h3 class="page-header">About</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
